9797ad88e31268b2aff31051ec0a5e8fcb7097cc
mrkwnzl <40534855+mrkwnzl@users.noreply.github.com>
2021-08-19 07:21:38 +0200

    Removed need for duplication of original function

--- free-alt.js
+++ free-alt.js
@@ -1,26 +1,12 @@
+let originalOnAlt;
+
 function _onAlt_Override(event, up, modifiers) {
   // Implementation replaced with no-op to avoid highlighting
 }
 
 function _handleKeys_Override(event, key, up) {
   if (this.hasFocus) return false;
-  if (key.toLowerCase() === "h") _onKeyH(event, up);
-}
-
-function _onKeyH(event, up) {
-  if ( !canvas?.ready ) return;
-  event.preventDefault();
-
-  // Highlight placeable objects on any layers which are visible
-  for ( let layer of canvas.layers ) {
-    if ( !layer.objects || !layer.interactiveChildren ) continue;
-    for ( let o of layer.placeables ) {
-      if ( !o.visible ) continue;
-      if ( !o.can(game.user, "hover") ) return;
-      if ( !up ) o._onHoverIn(event, {hoverOutOthers: false});
-      else o._onHoverOut(event);
-    }
-  }
+  if (key.toLowerCase() === "h") originalOnAlt(event, up);
 }
 
 Hooks.on("init", function () {
@@ -46,6 +32,7 @@ Hooks.on("init", function () {
 
   // Override default Foundry function for sliders
   if (game.settings.get("free-alt", "free-alt") || game.settings.get("free-alt", "set-world")) {
+    originalOnAlt = KeyboardManager.prototype._onAlt;
     KeyboardManager.prototype._onAlt = _onAlt_Override;
     const defaultHandleKeys = KeyboardManager.prototype._handleKeys;
     KeyboardManager.prototype._handleKeys = function(event, key, up) {
