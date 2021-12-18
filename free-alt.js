ff69ffc13bf814d3683df305d2083e485f435478
mrkwnzl <40534855+mrkwnzl@users.noreply.github.com>
2021-12-18 16:25:58 +0100

    Update free-alt.js

--- free-alt.js
+++ free-alt.js
@@ -1,3 +1,3 @@
 Hooks.on("ready", function () {
-  ui.notifications.error("Free Alt has been deprecated due to extended core functionality. If you want to free the alt key, use the Configure Controls menu. Free Alt can be deleted.")
+  ui.notifications.error("Free Alt has been deprecated due to expanded core functionality. If you want to free the alt key, use the Configure Controls menu. Free Alt should be deleted.")
 });
